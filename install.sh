7655e05afab575d772a474de6016a30949cccbce
Ragib Al Asad <ragibalasad@gmail.com>
2026-09-22 23:07:09 +0600

    Update repository URL in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-REPO_RAW="https://raw.githubusercontent.com/ragibalasad/warp-fix/main"
+REPO_RAW="https://raw.githubusercontent.com/ragibalasad/dotfiles/main"
 
 sudo curl -fsSL "$REPO_RAW/warpfix.sh" -o /usr/local/bin/warpfix
 sudo chmod +x /usr/local/bin/warpfix
